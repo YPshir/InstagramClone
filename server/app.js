c93846c124d6de53f4d88a3105a7af7b084240bd
YPshir <ypshir@gmail.com>
2020-07-13 15:28:37 +0300

    add auth

--- server/app.js
+++ server/app.js
@@ -4,6 +4,12 @@ const PORT=5000
 const mongoose=require('mongoose')
 const { MONGOURI }=require('./keys')
 
+require('./models/user')
+
+app.use(express.json())
+
+app.use(require('./routes/auth'))
+
 mongoose.connect(MONGOURI ,{ useNewUrlParser: true , useUnifiedTopology: true })
 mongoose.connection.on('connected',()=>{
     console.log("connected to mongo")
---
+++ server/routes/auth.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    res.send("hello")
+})
+
+router.post('/signup',(req,res)=>{
+    const {name,email,password}=req.body
+    if(!name || !email ||!password){
+        return res.status(422).json({error:"please add all the fields"})
+    }
+    res.json({message:"successfuly posted"})
+})
+
+module.exports = router
