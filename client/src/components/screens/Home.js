f20fe97ca6a4bdec823733fb6ed1cb765b2c0710
YPshir <ypshir@gmail.com>
2020-07-21 23:17:50 +0300

    create post page UI

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,44 @@ import React from 'react'
 
 const Home = ()=>{
     return(
-        <h1>Home</h1>
+        <div className="home">
+            <div className="card home-card"> 
+                <h5>David</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" />
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>Body - this is amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+            <div className="card home-card"> 
+                <h5>David</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" />
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>Body - this is amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+            <div className="card home-card"> 
+                <h5>David</h5>
+                <div className="card-image">
+                    <img src="https://images.unsplash.com/photo-1498550744921-75f79806b8a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60" />
+                </div>
+                <div className="card-content">
+                    <i className="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>Body - this is amazing post</p>
+                    <input type="text" placeholder="add a comment" />
+                </div>
+            </div>
+        </div>
     )
 }
 
--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -9,7 +9,7 @@ const Signin = ()=>{
                 <h2>Instagram</h2>
                 <input type="text" placeholder="email"/>
                 <input type="text" placeholder="password"/>
-                <button className="btn waves-effect waves-light #64b5f6 blue lighten-2">SignIN</button>
+                <button className="btn waves-effect waves-light #64b5f6 blue darken-1">SignIN</button>
                 <h5><Link to="/signup">Don't have an account?</Link></h5>
 
             </div>
