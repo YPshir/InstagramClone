b25bf4d2824cb2da9ec465d7ffb5fb87977e248b
YPshir <ypshir@gmail.com>
2020-08-17 09:27:56 +0300

    add populate

--- server/routes/post.js
+++ server/routes/post.js
@@ -52,7 +52,10 @@ router.put('/like',requireLogin,(req,res)=>{
         $push:{likes:req.user._id}
     },{
         new:true
-    }).exec((err,result)=>{
+    })
+    .populate("comments.postedBy","_id name")
+    .populate("postedBy","_id name")
+    .exec((err,result)=>{
         if (err){
             return res.status(422).json({error:err})
         }else{
@@ -65,7 +68,10 @@ router.put('/unlike',requireLogin,(req,res)=>{
         $pull:{likes:req.user._id}
     },{
         new:true
-    }).exec((err,result)=>{
+    })
+    .populate("comments.postedBy","_id name")
+    .populate("postedBy","_id name")
+    .exec((err,result)=>{
         if (err){
             return res.status(422).json({error:err})
         }else{
