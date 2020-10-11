42540aefab3b8c6ec28456877f4ee6b05aae89d7
YPshir <ypshir@gmail.com>
2020-10-11 16:39:13 +0300

    see posts of my following on client side

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Signup from './components/screens/Signup'
 import CreatePost from './components/screens/CreatePost'
 import {reducer,initialState} from './reducers/userReducer'
 import UserProfile from './components/screens/UserProfile';
+import SubscribesUserPosts from './components/screens/SubscribesUserPosts';
 
 export const UserContext = createContext()
 
@@ -44,6 +45,9 @@ const Routing = ()=>{
       </Route>
       <Route path="/profile/:userid">
         <UserProfile />
+      </Route>
+      <Route path="/myfollowingpost">
+        <SubscribesUserPosts />
       </Route>
     </Switch>
   )
