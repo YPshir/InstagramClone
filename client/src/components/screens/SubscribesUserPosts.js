d3af35940239e69910fa8dd36118d7f7f1c3e113
YPshir <ypshir@gmail.com>
2020-10-11 17:47:50 +0300

    add styling

--- client/src/components/screens/SubscribesUserPosts.js
+++ client/src/components/screens/SubscribesUserPosts.js
@@ -118,7 +118,7 @@ const SubscribesUserPosts = ()=>{
                 data.map(item=>{
                     return(
                         <div className="card home-card" key={item._id}> 
-                        <h5> <Link to={item.postedBy._id!==state._id ? "/profile/" + item.postedBy._id : "/profile/"}>{item.postedBy.name}</Link>
+                        <h5 style={{padding:"5px"}}><Link to={item.postedBy._id!==state._id ? "/profile/" + item.postedBy._id : "/profile/"}>{item.postedBy.name}</Link>
                             {item.postedBy._id==state._id 
                             && <i className="material-icons" style={{float:"right"}}
                             onClick={()=>deletePost(item._id)}
