e7dce81aca2b9752ee3ce7ed9e54660bdd9053f1
Ypshir <ypshir@gmail.com>
2020-11-07 21:41:36 +0200

    add profile pic in posts in home page

---
+++ README.md
@@ -0,0 +1,33 @@
+# InstagramClone  
+
+Developing an Instagram web app clone using MERN Stack (MERN stands for MongoDB, Express.js, React.js and Node.js) and more features.
+
+## LIVE DEMO
+[LINK](https://instagram-clone-shir.herokuapp.com/) 
+
+## Preview
+// enter gif or photos  
+
+## Tech
+* Frontend: [ReactJS (Hooks)](https://reactjs.org/docs/hooks-intro.html)
+* Responsive CSS framwork: [Materialize](https://materializecss.com/)
+* State management: [React Context API](https://reactjs.org/docs/context.html)
+* Routing: [React Router hooks](https://reactrouter.com/web/api/Hooks)
+* Backend: [Node.js](https://nodejs.org/en/) & [Express](https://expressjs.com/)
+* Authentication and Authorization: [bcrypt.js](https://www.npmjs.com/package/bcryptjs) & [JWT based authentication](https://jwt.io/introduction/)
+* Database: [MongoDB](https://www.mongodb.com/)
+* Image hosting: [Cloudinary](https://cloudinary.com/)
+* Deploy: [Heroku](https://devcenter.heroku.com/)
+
+## Features
+- [x] User signup / login with a username and password
+- [x] One cannot enter the app without being authenticated
+- [x] User can upload / update his profile picture
+- [x] User can view his profile page and the profile page of other users
+- [x] Profile page of user contains profile picture, username, number of posts, number of followers, number of following and the posts
+- [x] Home page contains posts uploaded by all users registered for the app
+- [x] MyFollowingPost page contains posts uploaded by users that the logged in user is following
+- [x] User can create and delete post
+- [x] One cannot delete existing posts and comments created by other users
+- [x] User can follow / unfolloew other users
+- [x] User can like / unlike / comment other users' posts 
---
+++ client/src/components/screens/Signin.js
@@ -0,0 +1,62 @@
+import React,{useState,useContext} from 'react'
+import {Link,useHistory} from 'react-router-dom'
+import {UserContext} from '../../App'
+import M from 'materialize-css'
+
+const Signin = ()=>{
+    const {state,dispatch} = useContext(UserContext)
+    const history = useHistory()
+    const [email,setEmail]=useState("")
+    const [password,setPassword]=useState("")
+    const PostData = ()=>{
+        fetch("/signin", {
+            method:"post",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({
+                email,
+                password
+            })
+        }).then(res=>res.json())
+        .then(data=>{
+            console.log(data)
+            if(data.error){
+                M.toast({html: data.error, classes:"#c62828 red darken-3"})
+            }
+            else{
+                localStorage.setItem("jwt",data.token)
+                localStorage.setItem("user",JSON.stringify(data.user))
+                dispatch({type:"USER",payload:data.user})
+                M.toast({html: "signed in successfully", classes:"#43a047 green darken-1"})
+                history.push('/')
+            }
+        })
+        .catch(error=>{
+            console.log(error)
+        })
+    }
+    return(
+        <div className="mycard">
+            <div className="card auth-card input-field">
+                <h2>Instagram</h2>
+                <input type="text" placeholder="email"
+                    value={email}
+                    onChange={(e)=>setEmail(e.target.value)}
+                />
+                <input type="password" placeholder="password"
+                    value={password}
+                    onChange={(e)=>setPassword(e.target.value)}
+                />
+                <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+                    onClick={()=>PostData()}
+                >
+                    SignIN
+                </button>
+                <h5><Link to="/signup">Don't have an account?</Link></h5>
+            </div>
+        </div>
+    )
+}
+
+export default Signin
